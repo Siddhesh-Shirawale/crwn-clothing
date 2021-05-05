b16a3d18dbbbf4dcad36ed6c64a5f174a009fe7c
Siddhesh-Shirawale <siddhesh.shirawale@gmail.com>
2021-05-05 18:46:30 +0530

    added sign in form and styles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from "react-router-dom";
 
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 
@@ -13,6 +14,7 @@ function App() {
          <Switch>
             <Route exact path="/" component={Homepage} />
             <Route path="/shop" component={ShopPage} />
+            <Route path="/signin" component={SignInAndSignUpPage} />
          </Switch>
       </div>
    );
