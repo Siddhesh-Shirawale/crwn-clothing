463a16bc792cd0ac176472afa2434493834666ed
Siddhesh-Shirawale <siddhesh.shirawale@gmail.com>
2021-05-16 00:58:24 +0530

    remove gitIgnore

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,13 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 
-import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up-page/sign-in-and-sign-up.component";
 import ShopPage from "./pages/shop/shop.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 import Header from "./components/header/header.component";
+
+import { GlobalStyle } from "./global.styles";
 
 import { selectCurrentUser } from "./redux/user/users.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
@@ -21,6 +22,7 @@ const App = ({ checkUserSession, currentUser }) => {
 
    return (
       <div>
+         <GlobalStyle />
          <Header />
          <Switch>
             <Route exact path="/" component={Homepage} />
